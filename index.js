5c56180bc42a14358941b678fc3338a8b5b18491
florian M <florian110599@gmail.com>
2019-12-10 15:10:53 +0100

    4e

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 
-const express = require('express')
-const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
 
-app.get('/', function (req, res) {
-  res.send('Hello World !')
-})
+const express = require('express');
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({ extended: false })
+const apiRouter = require('./apiRouter').router;
+const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/',function (req,res){
+	res.setHeader('Content-Type','text/html');
+	res.status(200).send('<h1> Bonjour </h1>')
+});
+
+app.use('/api/',apiRouter)
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
 })
